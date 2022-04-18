6e8bd32930dcab1836951d38f59808b8b4b75e9a
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-18 23:31:54 +0300

    Implement method to conform to the test

--- src/classes/item.rb
+++ src/classes/item.rb
@@ -1,8 +1,15 @@
+require 'date'
+
 class Item
   attr_reader :published_date
+
   def initialize(id, published_date, archived)
     @id = id
     @published_date = published_date
     @archived = archived
+  end
+
+  def can_be_archived?
+    Date.today.year - @published_date.year >= 10
   end
 end
