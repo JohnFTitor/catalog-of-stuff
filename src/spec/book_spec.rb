b62744b7e2afeaa84703f47eeb4c8d319f449717
Zeeshan Haider <zhadier39@gmail.com>
2022-04-19 03:08:23 +0500

    Add unit tests for instantiation of Book and it's inheritance from Item

---
+++ src/spec/book_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../classes/book'
+
+describe Book do
+  before(:each) do
+    @book = Book.new('publisher', 'good')
+  end
+
+  context '#new' do
+    it 'is an instance of Book and takes two parameter' do
+      expect(@book).to be_instance_of(Book)
+    end
+
+    it 'gives Argument error when more or less than 3 params given' do
+      expect { Book.new('publisher') }.to raise_error(ArgumentError)
+    end
+
+    it 'is a child of Item' do
+      expect(@book).to be_kind_of(Item)
+    end
+  end
+end
