769acc4ce0498dfc1a62a7672c54c66881639596
Zeeshan Haider <zhadier39@gmail.com>
2022-04-19 03:40:32 +0500

    Add can_be_archived overriden method in Book to conform to tests

--- src/classes/book.rb
+++ src/classes/book.rb
@@ -3,8 +3,14 @@ require_relative './item.rb'
 class Book < Item
   attr_reader :publisher, :cover_state
 
-  def initialize(publisher, cover_state)
+  def initialize(publisher, cover_state, id, published_date, archived)
+    super(id, published_date, archived)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  private
+  def can_be_archived?
+    (@cover_state == 'bad' || super)
   end
 end
