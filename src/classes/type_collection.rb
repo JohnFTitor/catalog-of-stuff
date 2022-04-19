78c549849cd11cc8f3797fb11078853da239deb4
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-19 12:28:54 -0500

    Define json handler modules

--- src/classes/type_collection.rb
+++ src/classes/type_collection.rb
@@ -1,9 +1,9 @@
-class TypeCollection 
-  def initialize 
+class TypeCollection
+  def initialize
     @items = []
   end
 
-  def create
+  def get
     puts "[1] Select a #{self.class.name} from the list or [2] create a new #{self.class.name}"
   end
 
---
+++ src/modules/json_handler.rb
@@ -0,0 +1,24 @@
+require 'json'
+
+module JsonHandler
+  def write_json(path, arr)
+    opts = {
+      array_nl: "\n",
+      object_nl: "\n",
+      indent: '  ',
+      space_before: ' ',
+      space: ' '
+    }
+    File.open(path, 'w') do |file|
+      json = JSON.generate(arr, opts)
+      file.write(json)
+    end
+  end
+
+  def load_json(path)
+    return [] unless File.exist?(path)
+
+    read_path = File.read(path)
+    JSON.parse(read_path, create_additions: true)
+  end
+end
