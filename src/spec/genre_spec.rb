0aacfb272b69a071e70b67ee5db0815ee9d16138
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-19 15:23:11 -0500

    create failing test case for name property

--- src/spec/genre_spec.rb
+++ src/spec/genre_spec.rb
@@ -10,4 +10,15 @@ describe Genre do
       expect(@genre).to be_an_instance_of(Genre)
     end
   end
+
+  context 'Should have property name' do 
+    it 'should be able to read name' do 
+      name = @genre.name
+      expect(name).to eql 'Thriller'
+    end
+
+    it 'should not be able to write name' do 
+      expect {@genre.name = 'Something'}.to raise_error(NoMethodError)
+    end
+  end
 end
