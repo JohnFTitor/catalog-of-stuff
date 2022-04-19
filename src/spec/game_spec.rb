49646a842fa0a934f5ab5a68cc8fefa5c13eb041
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-19 20:54:12 +0300

    Add unit tests for the methods

--- src/spec/game_spec.rb
+++ src/spec/game_spec.rb
@@ -42,4 +42,26 @@ describe Game do
       expect(@game.last_played_at).to be_instance_of(Date)
     end
   end
+
+  context '#can_be_archived?' do
+    it 'should be private' do
+      expect { @game.can_be_archived? }.to raise_error(NoMethodError)
+    end
+  end
+
+  context '#move_to_archive' do
+    it "should return true when parent's method return true and last_played_at is older than 2 years" do
+      game = Game.new('multiplayer1', Date.new(2019, 2, 3), 1, Date.new(2001, 2, 3))
+      expect(game.move_to_archive).to be true
+    end
+
+    it "should return false when parent's method return false even if last_played_at is older than 2 years" do
+      game = Game.new('multiplayer1', Date.new(2021, 2, 3), 1, Date.new(2020, 2, 3))
+      expect(game.move_to_archive).to be false
+    end
+
+    it "should return false when last_played_at is smaller or equal 2 years even if parent's method return true" do
+      expect(@game.move_to_archive).to be false
+    end
+  end
 end
