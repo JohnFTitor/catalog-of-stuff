dd01733687b65436843b9b848f7b5ac2f32a13a8
Zeeshan Haider <zhadier39@gmail.com>
2022-04-20 03:41:17 +0500

    fix common checks module

--- src/classes/book_collection.rb
+++ src/classes/book_collection.rb
@@ -2,6 +2,7 @@ require_relative './book.rb'
 require_relative '../modules/common_input.rb'
 
 class BookCollection
+  def attr_reader @books
   include CommonInput
 
   def initialize
@@ -9,22 +10,25 @@ class BookCollection
   end
 
   def add(genre_coll, label_coll author_coll)
-    id = verify_id
-    publish_date = verify_publish_date
-    publisher = verify_publisher
+    id = verify_int(message: 'ID: ', error: 'Please input correct numeric id: ' )
+    publish_date = verify_date(message: 'Publish Date [yyyy-mm-dd]: ', error: 'Please input date in the correct format: ')
+    publisher = verify_publisher(message: "Publisher: ", error: "Please input a string: ")
     cover_state = verify_cover_state
     genre = genre_coll.get
     book_label = label_coll.get
     book_author = author_coll.get
-    new_book << Book.new(publisher, cover_state, id, publish_date)
+    new_book = Book.new(publisher, cover_state, id, publish_date)
+    new_book.genre= genre
+    new_book.author= author
+    new_book.label= label
+    @books << new_book
+  end
+
+  def list
+    puts @books
   end
 
   private
-  def verify_publisher
-    print "Publisher: "
-    publisher = gets.chomp.strip.capitalize
-  end
-
   def verify_cover_state
     print "How is the state of the cover? [Good/Bad] :"
     cover_state = gets.chomp.strip.downcase
@@ -34,4 +38,5 @@ class BookCollection
     end
     cover_state
   end
+
 end
