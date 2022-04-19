bb7685f95ad17608b161748e997a8e84b6e6e4f9
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-19 14:11:26 -0500

    Create test cases for spotify instance

--- src/spec/music_spec.rb
+++ src/spec/music_spec.rb
@@ -14,4 +14,14 @@ describe MusicAlbum do
       expect(@music).to be_a_kind_of(Item)
     end
   end
+
+  context 'Should have instance of spotify' do 
+    it 'instance can be read' do 
+      expect {@music.spotify}.to_not raise_error(MethodError)
+    end
+
+    it 'instance cannot be written' do 
+      expect {@music.spotify = true}.to raise_error(MethodError)
+    end
+  end
 end
