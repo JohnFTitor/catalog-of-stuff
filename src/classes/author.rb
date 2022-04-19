fa32ed03de47c8d9dab0ec15c424d666d3983076
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-20 00:38:24 +0300

    Add initialize method with the corresponding properties to pass the tests

--- src/classes/author.rb
+++ src/classes/author.rb
@@ -1,2 +1,10 @@
 class Author
+  attr_reader :first_name, :last_name
+
+  def initialize(id, first_name, last_name)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
 end
