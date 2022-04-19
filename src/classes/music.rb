d1ee88ed90a18972a4b7f93e78b198915ffecba8
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-19 14:27:13 -0500

    Add implementation that succed the test cases

--- src/classes/music.rb
+++ src/classes/music.rb
@@ -2,6 +2,11 @@ class MusicAlbum < Item
   attr_reader :spotify
 
   def initialize(spotify, id, date)
+    super(id, date)
     @spotify = spotify
+  end
+
+  def move_to_archive
+    false
   end
 end
