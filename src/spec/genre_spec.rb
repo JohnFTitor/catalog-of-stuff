35d24b83d1369d06f9f646a85bda0066064da6a1
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-19 16:54:48 -0500

    add additional test cases verifying privacy

--- src/spec/genre_spec.rb
+++ src/spec/genre_spec.rb
@@ -12,13 +12,27 @@ describe Genre do
   end
 
   context 'Property name' do 
-    it 'should be able to read name' do 
+    it 'should be able to be read' do 
       name = @genre.name
       expect(name).to eql 'Thriller'
     end
 
-    it 'should not be able to write name' do 
+    it 'should not be able to be written' do 
       expect {@genre.name = 'Something'}.to raise_error(NoMethodError)
+    end
+  end
+
+  context 'Property id' do 
+    it 'should not be accesible' do
+      expect { @genre.id }.to raise_error(NoMethodError)
+      expect { @genre.id = 1 }.to raise_error(NoMethodError)
+    end
+  end
+  
+  context 'Property items' do 
+    it 'should not be accesible' do
+      expect { @genre.items }.to raise_error(NoMethodError)
+      expect { @genre.items = [] }.to raise_error(NoMethodError)
     end
   end
 
