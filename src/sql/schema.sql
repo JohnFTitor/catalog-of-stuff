673b46afd3705ca024d3452e428cfd03cca4e4ab
Zeeshan Haider <zhadier39@gmail.com>
2022-04-20 22:13:04 +0500

    Add schema for book table

---
+++ src/sql/schema.sql
@@ -0,0 +1,21 @@
+/* Database schema to keep the structure of entire database. */
+-- to create the database: createdb catalog_of_stuff
+-- Book
+CREATE TABLE book(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  publish_date VARCHAR(100),
+  publisher DATE,
+  cover_state VARCHAR(4),
+  genre_id INT,
+  author_id INT,
+  label_id INT,
+  FOREIGN KEY(genre_id) references genre(id) ON DELETE
+  SET
+    NULL ON UPDATE CASCADE
+  FOREIGN KEY(author_id) references author(id) ON DELETE
+  SET
+    NULL ON UPDATE CASCADE
+  FOREIGN KEY(label_id) references label(id) ON DELETE
+  SET
+    NULL ON UPDATE CASCADE
+);
