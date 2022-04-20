90e58ef1dcc703864bb59f34d79483ffe730941a
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-20 20:39:16 +0300

    Add get method to AuthorCollection

--- src/classes/author_collection.rb
+++ src/classes/author_collection.rb
@@ -7,6 +7,17 @@ class AuthorCollection < TypeCollection
     create_defaults if @list.length.zero?
   end
 
+  def get
+    choice = super
+    if choice == 1
+      list
+      index = verify_index(@list)
+      @list[index]
+    else
+      create_author
+    end
+  end
+
   def list
     puts
     @list.each_with_index do |author, index|
@@ -15,6 +26,15 @@ class AuthorCollection < TypeCollection
   end
 
   private
+
+  def create_author
+    id = verify_int(message: 'Author ID: ', error: 'Please input correct numeric id: ')
+    first_name = verify_str(message: 'Author First Name: ', error: 'Please input a string for First Name: ')
+    last_name = verify_str(message: 'Author Last Name: ', error: 'Please input a string for Last Name: ')
+    author = Author.new(id, first_name, last_name)
+    @list << author
+    author
+  end
 
   def create_defaults
     author1 = Author.new(1, 'Stephen', 'King')
