b7db65e9c11355159cb2c5140a024ad6dede617d
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-20 20:53:49 +0300

    Add genre_coll

--- src/classes/game_collection.rb
+++ src/classes/game_collection.rb
@@ -8,7 +8,7 @@ class GameCollection
     @games = []
   end
 
-  def add(label_coll, author_coll)
+  def add(label_coll, author_coll, genre_coll)
     id = verify_int(message: 'ID: ', error: 'Please input correct numeric id: ')
     publish_date = verify_date(message: 'Publish Date [yyyy-mm-dd]: ',
                                error: 'Please input date in the correct format: ')
@@ -16,7 +16,7 @@ class GameCollection
     last_played_at = verify_date(message: 'Last Played Date [yyyy-mm-dd]: ',
                                  error: 'Please input date in the correct format: ')
     new_game = Game.new(multiplayer, last_played_at, id, publish_date)
-    # new_book.genre= genre_coll.get
+    new_game.genre = genre_coll.get
     new_game.author = author_coll.get
     new_game.label = label_coll.get
     @games << new_game
