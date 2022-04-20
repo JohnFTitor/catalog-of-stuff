0e6b3129c2d5361913623adab43e6698ec30bb16
Zeeshan Haider <zhadier39@gmail.com>
2022-04-20 06:49:58 +0500

    Add clear screen functionality

--- src/main.rb
+++ src/main.rb
@@ -2,21 +2,22 @@
 require_relative './classes/app'
 
 def main
-  puts 'Welcome to Catalog of my stuff!'
   app = App.new
   loop do
+    system("clear")
+    puts 'Welcome to Catalog of my stuff!'
     puts
     puts "Please choose an option by entering a number:
-           1 - List all books
-           2 - List all music albums
-           3 - List of games
-           4 - List all genres (e.g 'Comedy', 'Thriller')
-           5 - List all labels (e.g. 'Gift', 'New')
-           6 - List all authors (e.g. 'Stephen King')
-           7 - Add a book
-           8 - Add a music album
-           9 - Add a game
-          10 - Exit"
+  1 - List all books
+  2 - List all music albums
+  3 - List of games
+  4 - List all genres (e.g 'Comedy', 'Thriller')
+  5 - List all labels (e.g. 'Gift', 'New')
+  6 - List all authors (e.g. 'Stephen King')
+  7 - Add a book
+  8 - Add a music album
+  9 - Add a game
+  10 - Exit"
     choice = gets.chomp.to_i
 
     break if app.run(choice) == 'exit'
