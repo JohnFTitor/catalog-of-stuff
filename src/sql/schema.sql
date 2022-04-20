8b553993010718d7d0b0bae0abd4706db5b8c4f8
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-21 02:05:45 +0300

    Create Item table

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -19,3 +19,16 @@ CREATE TABLE label(
   title VARCHAR(100),
   color VARCHAR(100)
 );
+
+--Item
+CREATE TABLE item (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE,
+    archived BOOLEAN,
+    genre_id INT,
+    author_id INT,
+    label_id INT,
+    FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (label_id) REFERENCES label(id) ON DELETE SET NULL ON UPDATE CASCADE
+);
