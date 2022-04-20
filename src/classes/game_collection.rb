e55dc3f54ad102752f25c2b573f19f319ebf4dfa
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-20 20:49:24 +0300

    Add game item to game list

--- src/classes/game_collection.rb
+++ src/classes/game_collection.rb
@@ -8,7 +8,7 @@ class GameCollection
     @games = []
   end
 
-  def add(label_coll, author_coll, genre_coll)
+  def add(label_coll, author_coll)
     id = verify_int(message: 'ID: ', error: 'Please input correct numeric id: ')
     publish_date = verify_date(message: 'Publish Date [yyyy-mm-dd]: ',
                                error: 'Please input date in the correct format: ')
@@ -16,8 +16,20 @@ class GameCollection
     last_played_at = verify_date(message: 'Last Played Date [yyyy-mm-dd]: ',
                                  error: 'Please input date in the correct format: ')
     new_game = Game.new(multiplayer, last_played_at, id, publish_date)
-    new_game.genre = genre_coll.get
+    # new_book.genre= genre_coll.get
     new_game.author = author_coll.get
     new_game.label = label_coll.get
+    @games << new_game
+    print 'Game Created Successfully. Press enter to continue. '
+    gets.chomp
+  end
+
+  def list
+    system('clear')
+    @games.each_with_index do |game, index|
+      print "#{index}) Multiplayer: #{game.multiplayer}, Publish Date: #{game.published_date}, "
+      print "Last Played at: #{game.last_played_at}, Label: #{game.label.title} \n"
+      print "Author: #{game.author.first_name} #{game.author.last_name}\n"
+    end
   end
 end
