990e3383f31e16296f29c8e6021bfb330252aa1f
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-21 02:46:31 +0300

    Use pretty_generate() function to format json files

--- src/modules/json_handler.rb
+++ src/modules/json_handler.rb
@@ -3,15 +3,8 @@ require 'json/add/date'
 
 module JsonHandler
   def write_json(path, arr)
-    opts = {
-      array_nl: "\n",
-      object_nl: "\n",
-      indent: '  ',
-      space_before: ' ',
-      space: ' '
-    }
     File.open(path, 'w') do |file|
-      json = JSON.generate(arr, opts)
+      json = JSON.pretty_generate(arr)
       file.write(json)
     end
   end
