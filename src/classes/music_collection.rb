504c8c2903afca6cddb84269924f6c4961fa559e
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-19 22:17:46 -0500

    Define MusicCollection class

---
+++ src/classes/music_collection.rb
@@ -0,0 +1,18 @@
+require_relative './music'
+require_relative '../modules/common_checks'
+
+class MusicCollection
+  include CommonChecks
+
+  def initialize
+    @album = []
+  end
+
+  def add(_genre_collection, _label_collection)
+    id = verify_int(message: 'Please enter an ID: ', error: 'Wrong type. Please enter a valid integer')
+    published_date = verify_date(message: 'Enter a publish date as follows [yyyy-mm-dd]: ',
+                                 error: 'Wrong Format. Enter again [yyyy-mm-dd]: ')
+    on_spotify = yes_or_not(message: 'Is this track on Spotify (Y/N) ')                             
+    music = MusicAlbum.new(on_spotify, id, published_date)
+  end
+end
