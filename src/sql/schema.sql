0c13fc12bbcf9ce6d0730331f0b64643848ab0df
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-21 09:55:24 -0500

    Add genre and music album tables

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -19,3 +19,22 @@ CREATE TABLE label(
   title VARCHAR(100),
   color VARCHAR(100)
 );
+
+-- genre
+CREATE TABLE genre(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100),
+);
+
+-- Music Album
+CREATE TABLE MUSICALBUM(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  publish_date DATE,
+  on_spotify BOOLEAN,
+  genre_id INT,
+  author_id INT,
+  label_id INT,
+  FOREIGN KEY(genre_id) references genre(id) ON DELETE SET NULL ON UPDATE CASCADE
+  FOREIGN KEY(author_id) references author(id) ON DELETE SET NULL ON UPDATE CASCADE
+  FOREIGN KEY(label_id) references label(id) ON DELETE SET NULL ON UPDATE CASCADE
+);
